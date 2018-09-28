3da5e8f318d5fec3d5b51d7d0d62acf81a69303c
Hugo Christian <hugo@n2y.com.br>
2018-09-28 16:00:32 -0300

    Corrigido o problema para exibição dos documentos das aulas

--- app/pages/Documents/Content.php
+++ app/pages/Documents/Content.php
@@ -1,8 +1,6 @@
 <h2>Documentos das aulas</h2>
-<!--Será necessário refazer essa etapa, pois está com erros na exibição das aulas-->
-<!--Testando as sincronizações-->
+
 <?php
-echo $NomeLogado;
 $MatriculaLogada = $_SESSION['Login']['matricula'];
 $QueryAulasAssistidas = "SELECT * FROM usuarios WHERE matricula = '$MatriculaLogada'";
 $ExeQueryAulasAssistidas = mysql_query($QueryAulasAssistidas);
@@ -41,11 +39,11 @@ if ($ExeQueryAulasAssistidas) {
         if ($AulaAssistida8 == 1) {
             $AulaAssistida8 = 8;
         }
-
+        
         $QueryBuscarAulas = "SELECT * FROM aulas WHERE numero_aula = $AulaAssistida1 OR numero_aula = $AulaAssistida2 OR numero_aula = $AulaAssistida3 OR numero_aula = $AulaAssistida4 OR numero_aula = $AulaAssistida5 OR numero_aula = $AulaAssistida6 OR numero_aula = $AulaAssistida7 OR numero_aula = $AulaAssistida8";
         $ExeQrBuscarAulas = mysql_query($QueryBuscarAulas);
 
-        if (mysql_num_rows($ExeQrBuscarAulas > 0)) {
+        
             while ($DadosAulas = mysql_fetch_assoc($ExeQrBuscarAulas)) {
                 ?>
                 <article class="col-md-12">
@@ -56,17 +54,8 @@ if ($ExeQueryAulasAssistidas) {
                 <hr>
                 <?php
             }
-        } else {
-            ?>
-            <article class="col-md-12">
-                <img src="<?php echo $DadosAulas['img_docs'] ?>" title="" alt="" class="img-responsive img-rounded img-bordered col-xs-12 col-sm-8 col-md-6 col-lg-6">
-                <p class="lead hidden-xs col-sm-4 col-md-6 col-lg-6" style="padding-top:12.5%;"><a href="<?php echo $DadosAulas['link_documento']; ?>" target="new">Aula <?php echo $DadosAulas['numero_aula'] ?> - <?php echo $DadosAulas['titulo_aula'] ?></a></p>
-            </article>
-            <div class="clearfix"></div>
-            <hr>
-            <?php
-        }
+        
     }
 } else {
-    echo "Deu ruim!";
+    echo "Ocorreu um erro ao tentar carregar o material para o aluno, entre em contato com o administrador do sistema. Código do erro: ".mysql_error();
 }
