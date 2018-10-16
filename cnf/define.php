403911263afe5d6ba7724c130ac83185dab4c3a2
Hugo Christian <hugo@n2y.com.br>
2018-10-16 17:13:46 -0300

    Bug de conexão ao banco de dados corrigido

--- cnf/define.php
+++ cnf/define.php
@@ -4,23 +4,16 @@
 $HOST = "localhost";
 $USER = "root";
 $PASS = "";
-$DDB = "experience";
+$DDB = "n2yco435_experience";
 
-$conexao = mysqli_connect($HOST, $USER, $PASS, $DDB) ;
+$conexao = mysql_connect($HOST, $USER, $PASS)or die(mysql_error);
+mysql_select_db($DDB, $conexao) or die(mysql_error);
 
 if(!$conexao){
     echo "Impossível se conectar ao MySQL. ".PHP_EOL."<br>";
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging errno: " . mysql_connect_errno() . PHP_EOL;
+    echo "Debugging errno: " . mysql_connect_errno() . PHP_EOL;
     exit;
-}else{
-    ?>
-    <p style="width:80%; padding: 5px 1.5% 5px 18.5%;text-align:center">
-        Sistema conectado ao Banco de Dados <?php echo PHP_EOL?>
-        <br>
-        Informação do Servidor: <?php echo mysqli_get_host_info($conexao) . PHP_EOL ?>
-    </p>
-    <?php
 }
 /*
 //On-Line HOSTGATOR
