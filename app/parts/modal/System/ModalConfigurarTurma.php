d0e0a39b81845ac841d1bff933c5052b9dbfaa22
Hugo Christian <hugo@n2y.com.br>
2018-10-26 14:13:16 -0300

    Exibição do nome do professor no modal de configuração da turma

--- app/parts/modal/System/ModalConfigurarTurma.php
+++ app/parts/modal/System/ModalConfigurarTurma.php
@@ -9,13 +9,38 @@
                     <h4 class="modal-title">Configurar turma: <?php echo $ReturnTurmas['nome_turma']?></h4>
                 </div>
                 <div class="modal-body">
-                    <div class="formgroup"></div>
-                    <div class="formgroup"></div>
+                    <div class="formgroup">
+                        <div class="col-md-4">
+                            <label for="HorarioEntrada">Horario de Entrada:</label>
+                            <input type="text" value="<?php echo $ReturnTurmas['horario_entrada_turma']?>:00" disabled class="form-control">
+                        </div>
+                        <div class="col-md-4">
+                            <label for="HorarioSaida">Horario de Saída:</label>
+                            <input type="text" value="<?php echo $ReturnTurmas['horario_saida_turma']?>:00" disabled class="form-control">
+                        </div>
+                        <div class="col-md-4">
+                            <label for="Professor">Professor:</label>
+                            <?php 
+                                $Professor = $ReturnTurmas['professor_turma'];
+                                $QueryBuscarNomeProfessor = "SELECT * FROM usuarios where id = '$Professor'";
+                                $ExeQrBuscarNomeProfessor = mysql_query($QueryBuscarNomeProfessor);
+                                while($ReturnNome = mysql_fetch_assoc($ExeQrBuscarNomeProfessor)){
+                                    ?>
+                                    <input type="text" value="<?php echo $ReturnNome['nome']?>" disabled class="form-control">
+                                    <?php
+                                }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="formgroup">
+                        
+                    </div>
                     <div class="formgroup"></div>
                     <div class="formgroup"></div>
                     <div class="formgroup"></div>
                     <div class="formgroup"></div>
                 </div>
+                <div class="clearfix" style="margin-bottom:20px"></div>
                 <div class="modal-footer">
                     <input type="hidden" name="IdTurma" value="<?php echo $ReturnTurmas['id']?>">
                     <button class="btn btn-default" data-dismiss="modal">Fechar</button>
