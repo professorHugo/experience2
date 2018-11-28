1ed5d81364d6d452fe6f5b0160706096406adce8
Hugo Christian <hugo@n2y.com.br>
2018-11-28 10:29:45 -0200

    Update dos módulos finalizado

--- app/parts/modal/System/SalvarConfiguracaoTurma.php
+++ app/parts/modal/System/SalvarConfiguracaoTurma.php
@@ -4,16 +4,15 @@ echo "Salvando as alterações da turma: ". $_POST['NomeTurma'];
 echo "<br>";
 
 for($i=1;$i<=8;$i++){
-    echo $modulo = "modulo".$i;
-    echo " = ";
-    echo $Modulo[$i] = $_POST['Modulo'.$i];
-    echo "<br>";
-    echo $QueryUpdateModulos = "UPDATE turmas SET $modulo = '$Modulo[$i]' WHERE id = $Turma";
-    echo "<br>";
+    $modulo = "modulo".$i;
+    " = ";
+    $Modulo[$i] = $_POST['Modulo'.$i];
+    "<br>";
+    $QueryUpdateModulos = "UPDATE turmas SET $modulo = '$Modulo[$i]' WHERE id = $Turma";
+    "<br>";
     
     $ExeQrUpdateModulos = mysql_query($QueryUpdateModulos);
-    
-    if($ExeQrUpdateModulos){
-        echo "Update Realizado com sucesso";
-    }
+}
+if($ExeQrUpdateModulos){
+    echo "Update Realizado com sucesso";
 }
